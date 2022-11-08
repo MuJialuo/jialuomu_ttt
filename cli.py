454ca217a5a47d0db73d416d4d8da46d4d708ace
MuJialuo <50764706+MuJialuo@users.noreply.github.com>
2022-11-08 14:29:00 -0800

    first commit

---
+++ cli.py
@@ -0,0 +1,73 @@
+# This file contains the Command Line Interface (CLI) for
+# the Tic-Tac-Toe game. This is where input and output happens.
+# For core game logic, see logic.py.
+# Jialuo Mu, Nov 8, 2022, v0.1
+
+from logic import make_empty_board
+from logic import get_winner
+
+if __name__ == '__main__':
+    board = make_empty_board()
+    winner = None
+    turn = 0
+    now = None
+    move = 0
+
+    while winner == None:
+        # determine this turn's character
+        if (turn + 2) % 2 == 0:
+            now = 'X'
+        else:
+            now = 'O'
+
+        print("X: take a turn!")
+        print(board[0],'\n',board[1],'\n',board[2],'\n')
+        print('[1, 2, 3]\n[4, 5, 6]\n[7, 8, 9]')
+
+        move = input("your move (example: 1): ")
+        move = int(move)
+        #if move < 1 or move > 9:
+        #   print('input error, not in range')
+        if move == 1:
+            board[0][0] = now
+            print('move = ', move)
+        if move == 2:
+            board[0][1] = now
+            print('move = ', move)
+        if move == 3:
+            board[0][2] = now
+            print('move = ', move)
+        if move == 4:
+            board[1][0] = now
+            print('move = ', move)
+        if move == 5:
+            board[1][1] = now
+            print('move = ', move)
+        if move == 6:
+            board[1][2] = now
+            print('move = ', move)
+        if move == 7:
+            board[2][0] = now
+            print('move = ', move)
+        if move == 8:
+            board[2][1] = now
+            print('move = ', move)
+        if move == 9:
+            board[2][2] = now
+            print('move = ', move)
+        
+        # TODO: Show the board to the user.
+        # TODO: Input a move from the player.
+        # TODO: Update the board.
+        # TODO: Update who's turn it is. 
+
+        print(board[0],'\n',board[1],'\n',board[2],'\n')
+
+        print('Turn:', turn)
+        if turn > 1:
+            winner = get_winner(board, turn)
+        if winner != None:
+            print('winner is: ', winner, '!')
+        turn = turn + 1
+
+        # FIXME
---
+++ logic.py
@@ -0,0 +1,51 @@
+# This file is where game logic lives. No input
+# or output happens here. The logic in this file
+# should be unit-testable.
+
+
+def make_empty_board():
+    return [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None],
+    ]
+
+
+def get_winner(board, turn):
+    x = 0
+    y = 0 
+    letter_won = None
+    
+    #check
+    while x < 3:
+        if board[x][y] == board[x][y+1] == board[x][y+2]:
+            if board[x][y] != None:
+                letter_won = board[x][y]
+        x = x + 1
+    x = 0
+    while y < 3:
+        if board[x][y] == board[x+1][y] == board[x+2][y]:
+            if board[x][y] != None:
+                letter_won = board[x][y]
+        y = y + 1
+    if board[0][0] == board[1][1] == board[2][2]:
+        if board[0][0] != None:
+            letter_won = board[x][y]
+    elif board[2][0] == board[1][1] == board[0][2]:
+        if board[2][0] != None:
+            letter_won = board[x][y]
+    
+    print('letter_won', letter_won)
+
+    #output
+    if letter_won != 'Draw':
+        return letter_won
+    elif letter_won == 'Draw':
+        return None
+    elif turn == 8:
+        return letter_won
+
+
+def other_player(player):
+    """Given the character for a player, returns the other player."""
+    return "O"  # FIXME
